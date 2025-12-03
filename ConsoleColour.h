4b984cf8182acff014a9e2c74c73016e70180c13
Muhammad Ahmed <muhammad.ahmed.prof@gmail.com>
2025-12-04 04:14:06 +0500

    Delete ConsoleColour.h

--- ConsoleColour.h
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-// Sets the console text color (Windows only)
-// just used it to enhance visuals of the program somewhat
-
-
-inline void setColor(int color) {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);  //this fucntion is part of windows.h library
-}
